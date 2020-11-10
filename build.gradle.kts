6cb16678b41febe8cea33bb996b13103323f1105
Alec Muffett <alec.muffett@gmail.com>
2020-11-10 18:59:10 +0000

    commit: problem statement.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,35 @@ group = "me.alecm"
 version = "1.0-SNAPSHOT"
 description = "playground.wire.MainKt"
 
+// unlike:
+// https://github.com/Egorand/wire-multiplatform-sample/blob/master/protos/build.gradle.kts
+// I am hoping to keep this simple and neither embrace multiplatform
+// nor multi-project builds.  I am hoping to build something quite
+// small...
+
 plugins {
     kotlin("jvm") version "1.4.10"
     id("com.github.johnrengelman.shadow") version "6.1.0" // we don't need id("application") if using this?
+
+    // ATTEMPT #1
+    // id("com.squareup.wire")
+    // Plugin [id: 'com.squareup.wire'] was not found in any of the following sources:
+    // - Gradle Core Plugins (plugin is not in 'org.gradle' namespace)
+    // - Plugin Repositories (plugin dependency must include a version number for this source)
+
+    // ATTEMPT #2
+    // id("com.squareup.wire") version "3.5.0"
+    // Plugin [id: 'com.squareup.wire', version: '3.5.0'] was not found in any of the following sources:
+    // - Gradle Core Plugins (plugin is not in 'org.gradle' namespace)
+    // - Plugin Repositories (could not resolve plugin artifact 'com.squareup.wire:com.squareup.wire.gradle.plugin:3.5.0')
+    // Searched in the following repositories:
+    // MavenRepo
+    // Gradle Central Plugin Repository
+
 }
+
+// THIS IS NOT VIABLE UNTIL THE PLUGIN IS INTEGRATED
+// wire { kotlin { } }
 
 dependencies {
     Libs.implementations.forEach{ this.implementation(it) } // be verbose to avoid ambiguity
@@ -35,7 +60,7 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<ShadowJar> {
     manifest {
-        attributes["Main-Class"] = "playground.wire.HelloWorldKt"
+        attributes["Main-Class"] = "playground.wire.HelloWorldKt" // replaces application.setMainClassName stuff
     }
     minimize()
 }
